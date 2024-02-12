0c3b0bcf945ceb3f7101ff78b703e149bfa75e70
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-12 05:06:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-# NEED TO CITE CODE
-# https://uark.libguides.com/CSCE/CitingCode
-# https://www.geeksforgeeks.org/python-opencv-cv2-line-method/
-# cv2.line(image, start_point, end_point, color, thickness)
-# have a programming plan and glossary + technical algorithm
-# dodge and burn
-# field of view
-# maybe grayscale and then remove all darker colors (+ dodge and burn)
-# https://www.geeksforgeeks.org/filter-color-with-opencv/
-# https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/ for capturing video and editing
-# https://docs.opencv.org/4.x/d9/d61/tutorial_py_morphological_ops.html
-# maybe what I can do is cut out a part, edit it, and put it back onto the frame
-# https://www.geeksforgeeks.org/line-detection-python-opencv-houghline-method/
-# https://docs.opencv.org/3.4/d9/db0/tutorial_hough_lines.html
-
 # import required libraries
 import cv2
 import numpy as num
@@ -39,17 +24,9 @@ while True:
     rectangled = rectangle(frame)
     cropped = crop(optimized)
 
-    lines = cv2.HoughLines(cropped, 1, num.pi/180, 150)
+    lines = cv2.HoughLines(cropped, 1, num.pi/180, 200)
 
     final = drawing(lines, optimized)
-
-    # https://www.geeksforgeeks.org/filter-color-with-opencv/
-    # ranged = cv2.inRange(blurred, 0, 115)
-    # result = cv2.bitwise_and(blurred, blurred, mask=ranged)
-
-    # edges = cv2.Canny(result, 50, 150, apertureSize=3)
-
-    # https://learnopencv.com/cropping-an-image-using-opencv/
 
     cv2.imshow('Frame', final)
 
@@ -58,5 +35,3 @@ while True:
 
 video.release()
 cv2.destroyAllWindows()
-
-# import detectLines
