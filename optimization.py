9fc1dda2b480182290c9c91da3019aea9ffaeecc
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-12 05:08:29 -0500

    Update optimization.py

--- optimization.py
+++ optimization.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 def optimize(frame):
-    blurred = cv2.GaussianBlur(frame, (1, 1), 0)
-    canny = cv2.Canny(blurred, 100, 200)
+    blurred = cv2.GaussianBlur(frame, (5, 5), 0)
+    canny = cv2.Canny(blurred, 50, 150, 3)
 
     return canny
