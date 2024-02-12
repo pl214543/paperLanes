fb54495831116cd6b8c36a6b0338dd1db4244643
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-12 05:10:17 -0500

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,10 +1,13 @@
+# import required libraries
 import cv2
 import numpy as num
 
 def rectangle(frame):
 
+    # gets height and width of video
     height, width = frame.shape[:2]
 
+    # sets up required parameters for drawing rectangle
     topleftx = width * 0.175
     topleftx = int(topleftx)
     toplefty = height * 0.725
@@ -14,16 +17,17 @@ def rectangle(frame):
     bottomrighty = height * 0.25
     bottomrighty = int(bottomrighty)
 
+    # draws rectangle
     rectangled = cv2.rectangle(frame, (topleftx, toplefty), (bottomrightx, bottomrighty), 255, 3)
 
     return rectangled
 
 def crop(frame):
 
-    # https://www.tutorialspoint.com/how-to-mask-an-image-in-opencv-python
-
+    # gets height and width of video
     height, width = frame.shape[:2]
 
+    # sets up required parameters for drawing rectangles
     topleftx = width * 0.175
     topleftx = int(topleftx)
     toplefty = height * 0.725
@@ -33,9 +37,11 @@ def crop(frame):
     bottomrighty = height * 0.25
     bottomrighty = int(bottomrighty)
 
+    # creates a mask for cropping out outer edges
     mask = num.zeros(frame.shape[:2], num.uint8)
     mask[toplefty:bottomrighty, topleftx:bottomrightx] = 255
 
+    # crops using the mask
     cropped = cv2.bitwise_and(frame, frame, mask)
 
     return cropped
