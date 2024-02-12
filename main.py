88dd1af55f9fbc708887b3b28f2d5662ff7722ee
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-12 05:13:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,37 @@
 import cv2
 import numpy as num
 import math
+
+# import functions
 from reader import rectangle, crop
 from optimization import optimize
 from lineDraw import drawing
 
+# video capture
 video = cv2.VideoCapture(0)
 
+# check if the video is opened
 def checkVideo(video):
     if video.isOpened():
         print("Video will not open.")
 
 checkVideo(video)
 
-# https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/
 while True:
     booleanReady, frame = video.read()
 
+    # grayscale
     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # uses the functions from the other files to optimize, make a rectangle, and crop
     optimized = optimize(grey)
     rectangled = rectangle(frame)
     cropped = crop(optimized)
 
+    # detect the lines
     lines = cv2.HoughLines(cropped, 1, num.pi/180, 200)
 
+    # draw the final lines
     final = drawing(lines, optimized)
 
     cv2.imshow('Frame', final)
