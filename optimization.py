ff64f83dacc6e633663ad073c53acf9afc21a8ed
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-12 05:14:24 -0500

    Update optimization.py

--- optimization.py
+++ optimization.py
@@ -1,6 +1,8 @@
+# import required libraries
 import cv2
 import numpy as np
 
+# optimize using Gaussian Blur and Canny Edge
 def optimize(frame):
     blurred = cv2.GaussianBlur(frame, (5, 5), 0)
     canny = cv2.Canny(blurred, 50, 150, 3)
