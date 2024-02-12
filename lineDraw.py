a2f557100923b6050c7423a4a25f8e9158537391
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-12 05:08:12 -0500

    Update lineDraw.py

--- lineDraw.py
+++ lineDraw.py
@@ -1,1 +1,35 @@
+import cv2
+import numpy as num
 
+def drawing(frame, lines):
+    for r_theta in lines:
+        arr = num.array(r_theta[0], dtype=num.float64)
+        r, theta = arr
+        # Stores the value of cos(theta) in a
+        a = num.cos(theta)
+
+        # Stores the value of sin(theta) in b
+        b = num.sin(theta)
+
+        # x0 stores the value rcos(theta)
+        x0 = a * r
+
+        # y0 stores the value rsin(theta)
+        y0 = b * r
+
+        # x1 stores the rounded off value of (rcos(theta)-1000sin(theta))
+        x1 = int(x0 + 1000 * (-b))
+
+        # y1 stores the rounded off value of (rsin(theta)+1000cos(theta))
+        y1 = int(y0 + 1000 * (a))
+
+        # x2 stores the rounded off value of (rcos(theta)+1000sin(theta))
+        x2 = int(x0 - 1000 * (-b))
+
+        # y2 stores the rounded off value of (rsin(theta)-1000cos(theta))
+        y2 = int(y0 - 1000 * (a))
+
+        # cv2.line draws a line in img from the point(x1,y1) to (x2,y2).
+        # (0,0,255) denotes the colour of the line to be
+        # drawn. In this case, it is red.
+        cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
