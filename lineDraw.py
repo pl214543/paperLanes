a63982ba6905ec1743d41499eb734e7caf2c498f
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-25 11:54:30 -0500

    Update lineDraw.py

--- lineDraw.py
+++ lineDraw.py
@@ -14,42 +14,42 @@ def drawing(frame, lines):
     # checks if anything is in the list of lines (do any lines exist) so no errors appear
     if lines is not None:
 
-        parallels = findParallels(lines, frame)
+        parallels = findParallels(lines)
+
+        print(parallels)
 
         if len(parallels) != 0:
             for parLines in parallels:
+                print(parLines)
                 for line in parLines:
+                    print(line[0][0])
                     averageStartX += line[0][0]
                     averageEndX += line[0][2]
                     averageStartY += line[0][1]
                     averageEndY += line[0][3]
 
-            averageStartX = averageStartX.item()
-            averageEndX = averageEndX.item()
-            averageStartY = averageStartY.item()
-            averageEndY = averageEndY.item()
+                averageStartX = averageStartX.item()
+                averageEndX = averageEndX.item()
+                averageStartY = averageStartY.item()
+                averageEndY = averageEndY.item()
 
-            averageStartX = averageStartX / int(len(parallels))
-            averageEndX = averageEndX / int(len(parallels))
-            averageStartY = averageStartY / int(len(parallels))
-            averageEndY = averageEndY / int(len(parallels))
+                averageStartX = averageStartX / 2
+                averageEndX = averageEndX / 2
+                averageStartY = averageStartY / 2
+                averageEndY = averageEndY / 2
 
-            averageStartX = int(averageStartX)
-            averageEndX = int(averageEndX)
-            averageStartY = int(averageStartY)
-            averageEndY = int(averageEndY)
+                averageStartX = int(averageStartX)
+                averageEndX = int(averageEndX)
+                averageStartY = int(averageStartY)
+                averageEndY = int(averageEndY)
 
-            print("\n\n\n\n\n" + str(averageStartX) + "\n\n\n\n\n")
-
-            cv2.line(frame, (averageStartX, averageStartY), (averageEndX, averageEndY), (0, 255, 0), 2)
+                cv2.line(frame, (averageStartX, averageStartY), (averageEndX, averageEndY), (0, 255, 0), 2)
 
         for parLines in parallels:
-            pass
+            for line in parLines:
+                x1, y1, x2, y2 = line[0]
+                cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
 
     return frame
 
     # cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-
-
-    # returns the frame with lines drawn
-    return frame
