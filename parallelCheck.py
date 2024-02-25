e95ecb0d730d98dc66205e69fb6c09aaf3835fea
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-25 16:52:23 -0500

    Update parallelCheck.py

--- parallelCheck.py
+++ parallelCheck.py
@@ -36,31 +36,71 @@ def subAngles(angle1, angle2):
     # return the absolute value of the difference
     return finalAngle
 
+# finds the center of the line
+def findCenter(line):
+    
+    # unpacks the x and y values
+    x1, y1, x2, y2 = line[0]
+    
+    # gets the middle x
+    middleX = (x1 + x2) / 2
+    
+    # gets the middle y
+    middleY = (y1 + y2) / 2
+    
+    # returns the middle points
+    return [middleX, middleY]
+
+
+# checks the nearby mid points
+def nearbyCheck(line1, line2):
+
+    # gets the midpoints of each line
+    line1Center = findCenter(line1)
+    line2Center = findCenter(line2)
+
+    # compare center points
+    if abs(line1Center[0] - line2Center[0]) < 200:
+        return False
+    elif abs(line1Center[1] - line2Center[1]) < 225:
+        return False
+    else:
+        return True
+
 # function finds and saves parallel lines
 def findParallels(lines):
 
     # makes the list that will store the parallel lines
     parallels = []
+    nonparallels = []
 
     # iterates through the lines
     for i in range(len(lines)):
         for j in range(len(lines)):
             if i == j: continue
 
-            # gets the angle of the first line
-            angle1 = findAngle(lines[i])
+            # filters out lines that are too close
+            if nearbyCheck(lines[i], lines[j]) == True:
 
-            # gets the angle of the second line
-            angle2 = findAngle(lines[j])
+                # gets the angle of the first line
+                angle1 = findAngle(lines[i])
 
-            # gets the difference of the angles
-            difference = subAngles(angle1, angle2)
+                # gets the angle of the second line
+                angle2 = findAngle(lines[j])
 
-            # approximates the parallels because it won't be perfect
-            if 0 <= difference <= 2:
+                # gets the difference of the angles
+                difference = subAngles(angle1, angle2)
 
-                # the parallel lines will be appended
-                parallels.append([lines[i], lines[j]])
+                # approximates the parallels because it won't be perfect
+                if 0 <= difference <= 5:
+
+                    # the parallel lines will be appended
+                    parallels.append([lines[i], lines[j]])
+
+            else:
+
+                # adds lines that are not parallel
+                nonparallels.append([lines[i], lines[j]])
 
     # returns the list of parallel lines
-    return parallels
+    return parallels, nonparallels
